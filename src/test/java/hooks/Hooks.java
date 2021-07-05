7bce9ee3225df57a46c5c11991000937cc23f0dd
vsevolodsavin3000 <vsevolodsavin3000@gmail.com>
2021-07-05 15:54:01 +0300

    hooks edit

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,13 +1,19 @@
 package hooks;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hooks {
     @Before
     public void setUp() {
-        Selenide.clearBrowserCookies();
         Selenide.open("https://stage.hub.crtweb.ru/tracker");// https://hub.crtweb.ru/tracker
     }
 
+    @After
+    public void tearDown(){
+        Selenide.clearBrowserCookies();
+        Selenide.closeWindow();
+    }
 }
