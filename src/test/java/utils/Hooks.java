56fcc197e98782aa5b647176505d12b3c4be5ce5
Петрыч Николай <nip@crtweb.ru>
2021-08-06 15:31:29 +0500

    working logs on project

--- src/test/java/utils/Hooks.java
+++ src/test/java/utils/Hooks.java
@@ -4,18 +4,32 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
+import java.lang.invoke.MethodHandles;
 
 public class Hooks {
-    @Before
+    private static final Logger log = (Logger) LogManager.getLogger(MethodHandles.lookup().lookupClass());
+    Scenario scenario;
 
-    public void setUp() {
-      //  Configuration.holdBrowserOpen = true;
+    @Before
+    public void setUp(Scenario scenario) {
+        this.scenario = scenario;
+        log.debug("\n----\n" + scenario.getName() + "\n----");
         Selenide.open("https://stage.hub.crtweb.ru/tracker ");// https://hub.crtweb.ru/tracker
     }
 
 
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario) {
+        this.scenario = scenario;
+        if (scenario.isFailed()) {
+            log.error(scenario.getName() + " Failed ");
+        } else {
+            log.info(scenario.getName() + " Success ");
+        }
         Selenide.clearBrowserCookies();
         Selenide.closeWindow();
     }
