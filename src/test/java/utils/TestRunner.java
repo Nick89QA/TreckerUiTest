7cfde1cdcd42d1aaba662637b53683914f6428fa
Петрыч Николай <nip@crtweb.ru>
2021-09-03 13:17:07 +0500

    Add tags

--- src/test/java/utils/TestRunner.java
+++ src/test/java/utils/TestRunner.java
@@ -2,15 +2,11 @@ package utils;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.lang.invoke.MethodHandles;
 
 @CucumberOptions(
         features = {"src/test/resources/features"},
-        glue = {"stepdefs", "utils"},// обязательно  hooks,testRunner должны быть в папке untils
-        tags = "@All"//аннотация по которому запускается тест с консоли командой mvn clean test
+        glue = {"stepdefs", "utils"},// обязательно  hooks,testRunner должны быть в папке utils
+        tags = "@All" //аннотация по которому запускается тест с консоли командой mvn clean test
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
