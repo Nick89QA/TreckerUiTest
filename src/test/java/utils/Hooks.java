6029bc16a98b9fe405ab67834f98c06793553994
nip <nip@crtweb.ru>
2021-09-14 13:11:44 +0500

    New fix

--- src/test/java/utils/Hooks.java
+++ src/test/java/utils/Hooks.java
@@ -8,19 +8,27 @@ import io.cucumber.java.Scenario;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.lang.invoke.MethodHandles;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class Hooks {
     private static final Logger log = (Logger) LogManager.getLogger(MethodHandles.lookup().lookupClass());
     Scenario scenario;
 
     @Before
-    public void setUp(Scenario scenario) {
+    public void setUp(Scenario scenario) throws MalformedURLException {
         this.scenario = scenario;
         log.debug("\n----\n" + scenario.getName() + "\n----");
-
-
+//        URL url = new URL("http://localhost:4444");
+//        DesiredCapabilities dc = DesiredCapabilities.chrome();
+//        RemoteWebDriver driver = new RemoteWebDriver(url,dc);
+//        driver.get("https://stage.hub.crtweb.ru/tracker ");
+//        System.out.println("Title of page:" +driver.getTitle());
         Selenide.open("https://stage.hub.crtweb.ru/tracker ");// https://hub.crtweb.ru/tracker
     }
 
